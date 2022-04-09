2c8fd297f538ebe3c0056900cd44530de5e38217
LAPTOP-AFFLLPMS\kimju <kimjunho1231@naver.com>
2022-04-10 00:11:38 +0900

    registration page user input add and radio function incomplete

--- main.dart
+++ main.dart
@@ -87,23 +87,177 @@ class Rabbitcalmain extends StatelessWidget {
     );
   }
 }
-
-class Rabbitcal_regitstraion extends StatelessWidget {
+class Rabbitcal_regitstraion extends StatefulWidget {
   const Rabbitcal_regitstraion({Key? key}) : super(key: key);
+
+  @override
+  State<Rabbitcal_regitstraion> createState() => _Rabbitcal_regitstraionState();
+
+}
+
+enum Gender {Man,Women}
+
+class _Rabbitcal_regitstraionState extends State<Rabbitcal_regitstraion> {
+
+  int _counter=0;
+   Gender _gender =Gender.Man;
+
+   void _incrementCounter() {
+     setState(() {
+       _counter++;
+     });
+   }
+    String inputs="";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Second Route"),
-      ),
-      body:Center(
-        child: RaisedButton(
-          onPressed:(){
-Navigator.pop(context);
-          },
-        )
-      )
+        appBar: AppBar(
+          title:Text('Rabbitcal',style:TextStyle(
+            fontWeight:FontWeight.bold,
+            fontSize: 40,
+          ),
+          ),
+        ),
+        body:Center(
+          child:Column(
+              children:<Widget>[
+
+              ListTile(
+                title:Text('남자'),
+                leading: Radio(
+                  value:Gender.Man,
+                  groupValue: _gender,
+                  onChanged:(value){
+                    setState(()
+                    {
+                      _gender=value;
+                    });
+                  },
+                ),
+              ),
+
+                ListTile(
+                  title:Text('여자'),
+                  leading: Radio(
+                    value:Gender.Women,
+                    groupValue: _gender,
+                    onChanged: (value){
+                      setState((){
+                        _gender=value;
+                      }
+                      );
+                    },
+                  ),
+                ),
+
+                Container(
+
+                  width:300,height:70,
+                  child: TextField(
+                    style: TextStyle(fontSize:32),
+                    textAlign:TextAlign.center,
+                    decoration: InputDecoration(hintText:'키'),
+                    onChanged: (String str){
+                      setState(()=>inputs =str);
+                    },
+                  ),
+                ),
+                Container(
+
+                  width:300,height:70,
+                  child: TextField(
+                    style: TextStyle(fontSize:32),
+                    textAlign:TextAlign.center,
+                    decoration: InputDecoration(hintText:'몸무게'),
+                    onChanged: (String str){
+                      setState(()=>inputs =str);
+                    },
+                  ),
+                ),
+                Container(
+
+                  width:300,height:70,
+                  child: TextField(
+                    style: TextStyle(fontSize:32),
+                    textAlign:TextAlign.center,
+                    decoration: InputDecoration(hintText:'오른쪽 팔 골격근량'),
+                    onChanged: (String str){
+                      setState(()=>inputs =str);
+                    },
+                  ),
+                ),
+                Container(
+
+                  width:300,height:70,
+                  child: TextField(
+                    style: TextStyle(fontSize:32),
+                    textAlign:TextAlign.center,
+                    decoration: InputDecoration(hintText:'왼쪽 팔 골격근량'),
+                    onChanged: (String str){
+                      setState(()=>inputs =str);
+                    },
+                  ),
+                ),
+                Container(
+
+                  width:300,height:70,
+                  child: TextField(
+                    style: TextStyle(fontSize:32),
+                    textAlign:TextAlign.center,
+                    decoration: InputDecoration(hintText:'오른쪽 다리 골격근량'),
+                    onChanged: (String str){
+                      setState(()=>inputs =str);
+                    },
+                  ),
+                ),
+                Container(
+
+                  width:300,height:70,
+                  child: TextField(
+                    style: TextStyle(fontSize:32),
+                    textAlign:TextAlign.center,
+                    decoration: InputDecoration(hintText:'왼쪽 다리 골격근량'),
+                    onChanged: (String str){
+                      setState(()=>inputs =str);
+                    },
+                  ),
+                ),
+                Container(
+
+                  width:300,height:70,
+                  child: TextField(
+                    style: TextStyle(fontSize:32),
+                    textAlign:TextAlign.center,
+                    decoration: InputDecoration(hintText:'복부 골격근량'),
+                    onChanged: (String str){
+                      setState(()=>inputs =str);
+                    },
+                  ),
+                ),
+                Container(
+
+                  width:300,height:70,
+                  child: TextField(
+                    style: TextStyle(fontSize:32),
+                    textAlign:TextAlign.center,
+                    decoration: InputDecoration(hintText:'체지방량'),
+                    onChanged: (String str){
+                      setState(()=>inputs =str);
+                    },
+                  ),
+                ),
+
+                Container(
+                  child: RaisedButton(
+                    onPressed:(){
+                      Navigator.pop(context);
+                    },
+                  ),
+                )
+              ]
+          )
+            )
     );
   }
 }
