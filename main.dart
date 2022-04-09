10f9909fa940e39ee6359bdda8dd1a71f36206fc
LAPTOP-AFFLLPMS\kimju <kimjunho1231@naver.com>
2022-04-09 21:46:15 +0900

    change button and font size and click to add page movement

--- main.dart
+++ main.dart
@@ -16,46 +16,68 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: Rabbitcalmain(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class Rabbitcalmain extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar:AppBar(
-        title:Text('Rabbitcal'),
+        title:Text('Rabbitcal',style:TextStyle(
+        fontWeight:FontWeight.bold,
+        fontSize: 40,
+    ),
+      ),
       ),
           body: Center(
             child: Container(
               padding: EdgeInsets.all(10),
               margin:EdgeInsets.all(10),
               child: Column(
-                children:[
+                children:<Widget>[
 
-                  Image.asset('Rabbitcal.jpg',width:400,height:400),
+                  Container(
+                      child: Image.asset('Rabbitcal.jpg',width:400,height:400)
+                  ),
 
                     Container(
+                      width:250,height:100,
                       padding: EdgeInsets.all(10),
                       margin:EdgeInsets.all(10),
                       child: RaisedButton(
-                        child: Text('등록'),
+                        child: Text('인바디 등록',style:TextStyle(
+                          fontWeight:FontWeight.bold,
+                          fontSize: 30,
+                        ),),
                         color:Colors.white,
-                        onPressed: () {},
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder:(context)=>Rabbitcal_regitstraion())
+                          );
+                        },
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        )
                   ),
                     ),
                     Container(
+                      width:250,height:100,
                           padding: EdgeInsets.all(10),
                           margin:EdgeInsets.all(10),
                       child: RaisedButton(
-                        style: RaisedButton.styleFrom(
-
-                        ),
-                          child: Text('추천'),
+                          child: Text('운동 추천',style:TextStyle(
+                            fontWeight:FontWeight.bold,
+                            fontSize: 30,
+                          ),),
                           color:Colors.white,
-                          onPressed: () {}
+                          onPressed: () {},
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
                       ),
                     ),
                   ]
@@ -66,3 +88,22 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
+class Rabbitcal_regitstraion extends StatelessWidget {
+  const Rabbitcal_regitstraion({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body:Center(
+        child: RaisedButton(
+          onPressed:(){
+Navigator.pop(context);
+          },
+        )
+      )
+    );
+  }
+}
