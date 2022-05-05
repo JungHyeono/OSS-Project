e6732432763c40bfef763fb9741991dde783e854
juhojang <juhojang@hotmail.com>
2022-05-05 23:20:31 +0900

    framework of loginpage

--- loginpage.dart
+++ loginpage.dart
@@ -1,33 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main()=>runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const login_page(),
-    );
-  }
-}
-
-class login_page extends StatelessWidget {
-  const login_page({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("login page demo"),
+      home: Scaffold(
+        body: Stack(
+          children: [
+            Container(color: Colors.red,),
+            Column(
+              children: [
+                Container(width: 200,height: 200,color: Colors.blue,),
+                Stack(children: [
+                  Container(width: 200,height: 200,color: Colors.amber,),
+                  Container(width: 200,height: 200,color: Colors.black,),
+                ],),
+                Container(width: 200,height: 200,color: Colors.white,),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
