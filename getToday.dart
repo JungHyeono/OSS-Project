eca5181289060d2f69583ca005c6dc2578ba801c
juhojang <juhojang@hotmail.com>
2022-05-06 00:37:07 +0900

    add username for motivation

--- getToday.dart
+++ getToday.dart
@@ -1,7 +1,9 @@
 import 'package:intl/intl.dart';
 
 void main() {
-  print(getToday());
+  List<String> date=[];
+  date.add(getToday());
+  print(userName(date.length));
 }
 
 
@@ -10,4 +12,20 @@ String getToday(){
   DateFormat formatter=DateFormat('yyyy-MM-dd');
   String today=formatter.format(now);
   return today;
+}
+
+String userName(int num){
+  if(num>=0 && num<=10)
+    return '헬린이';
+  else if(num>10 && num<=20)
+    return '헬중딩';
+  else if(num>20 && num<=30)
+    return '헬고딩';
+  else if(num>30 && num<=40)
+    return '헬대딩';
+  else if(num>40 && num<=50)
+    return '헬직딩';
+  else if(num>60 && num<=70)
+    return '운동성애자';
+  return "헬창";
 }
