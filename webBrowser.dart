a8cd3a479be5f0a59a6611582cccf433ff3525f8
juhojang <juhojang@hotmail.com>
2022-05-06 22:10:46 +0900

    function of webBrowser

---
+++ webBrowser.dart
@@ -0,0 +1,14 @@
+
+
+Future<void> _launchInBrowser(String url) async {
+  if (await canLaunch(url)) {
+    await launch(
+      url,
+      forceSafariVC: false,
+      forceWebView: false,
+      headers: <String, String>{'my_header_key': 'my_header_value'},
+    );
+  } else {
+    throw '웹 호출 실패 $url';
+  }
+}
