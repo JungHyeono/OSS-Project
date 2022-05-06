b63b5a051a228f0d9f9ff226cb0227f47e7ce112
juhojang <juhojang@hotmail.com>
2022-05-06 22:17:24 +0900

    add fuction of webBrowser and example of veryDetailPage

--- recommandExercise.dart
+++ recommandExercise.dart
@@ -217,3 +217,96 @@ class detail3 extends StatelessWidget {
   }
 }
 
+Future<void> _launchInBrowser(String url) async {
+  if (await canLaunch(url)) {
+    await launch(
+      url,
+      forceSafariVC: false,
+      forceWebView: false,
+      headers: <String, String>{'my_header_key': 'my_header_value'},
+    );
+  } else {
+    throw '웹 호출 실패 $url';
+  }
+}
+
+class veryDetail1_1 extends StatelessWidget {
+  const veryDetail1_1({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("사용자님에 맞는 이두근육 운동법"),
+        ),
+        body: Column(
+            children: [
+              Row(children: <Widget>[
+                Text(
+                  "이두근육이란?",
+                  style: TextStyle(fontSize: 30),
+                ),
+              ]),
+              Row(children: <Widget>[
+                Text(
+                  " ",
+                  style: TextStyle(fontSize: 30),
+                ),
+              ]),
+              Row(children: <Widget>[
+                Image.asset(
+                  'images/idu.png',
+                  fit: BoxFit.fill,
+                  width: 300,
+                  height: 300,
+                ),
+                Flexible(
+                    child: RichText(
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 5,
+                        strutStyle: StrutStyle(fontSize: 16.0),
+                        text: TextSpan(
+                            text:
+                            '하나의 근육(筋肉)이 두 개의 뼈의 부분으로부터 시작되어 그 머리가 둘로 갈라진 근육. 이를테면, 위팔 이두근·넓적다리 이두근 따위.',
+                            style: TextStyle(fontSize: 17)))),
+              ]
+              ),
+              Row(children: [
+                Expanded(child: Container(
+                  color: Colors.black, width: 100, height: 5,))
+              ],),
+              Row(children: [
+                Text(
+                  '사용자님에게 추천하는 운동은 저강도 운동입니다!', style: TextStyle(fontSize: 30),)
+              ],),
+              Row(children: [
+                Expanded(child:
+                GestureDetector(
+                  onTap: (){
+                    _launchInBrowser('https://www.youtube.com');
+                  },
+                  child: Container(color: Colors.white, width: 100, height: 50,
+                    child: Row(
+                      children: [
+                        Column(
+                          children: [
+                            Image.asset('images/youtubelogo.png',fit: BoxFit.fill,width: 35,height: 30,)
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Center(child:
+                        Text("유튜브 바로가기", style: TextStyle(fontSize: 20)))
+                          ],
+                        ),
+                      ],),
+                  ),
+                ),),
+              ],
+              ),
+            ])
+    );
+  }
+}
+
