656ba684f34da5487d0f69cec883bad8ec981234
juhojang <juhojang@hotmail.com>
2022-05-07 17:28:06 +0900

    add input card and login button

--- loginpage.dart
+++ loginpage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 void main()=>runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -17,31 +16,65 @@ class MyApp extends StatelessWidget {
         body: Stack(
           alignment: Alignment.center,
           children: [
-            Container(color: Colors.lightBlueAccent,),
+            Container(color: Colors.blue[50],),
             Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Container(width: 200,height: 200,color: Colors.blue,),
+                Container(height: 50,),
                 Stack(children: [
-                  Form(
-                    key: _formKey,
-                      child: Column(
-                    children: [
-                      TextFormField(
-                        controller: _emailController,
-                        decoration: InputDecoration(icon: Icon(Icons.account_circle),labelText: "Email"),
-                      ),
-                      TextFormField(
-                        controller: _passwordController,
-                      ),
-                      Container(height: 20,),
-                      Text("비밀번호를 잊어버리셨나요?"),
-                    ],
-                  )),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Card(
+                      elevation: 10,
+                      child: Form(
+                        key: _formKey,
+                          child: Column(
+                        children: [
+                          TextFormField(
+                            controller: _emailController,
+                            decoration: InputDecoration(icon: Icon(Icons.account_circle),labelText: "이메일"),
+                            validator: (value){
+                              if(value==' '){
+                                return "올바르지 않는 이메일입니다.";
+                              }
+                              return null;
+                            },
+                          ),
+                          TextFormField(
+                            controller: _passwordController,
+                            decoration: InputDecoration(icon: Icon(Icons.vpn_key),labelText: "비밀번호"),
+                            validator: (value){
+                              if(value==' '){
+                                return "올바르지 않는 비밀번호입니다.";
+                              }
+                              return null;
+                            },
+                          ),
+                          Container(height: 10,),
+                          Text("비밀번호를 잊어버리셨나요?"),
+                          Container(height: 10,),
+                        ],
+                      )),
+                    ),
+                  ),
                   Container(width: 100,height: 50),
                 ],),
-                Container(height:50,),
+                Positioned(
+                  left:10,
+                  right: 10,
+                  bottom: 0,
+                  child: RaisedButton(
+                      child: Text("login"),
+                      color: Colors.blue[100],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      onPressed: (){}
+                  ),
+                ),
+                Container(height:10,),
                 Text("계정이 없으신가요? 하나 만들어보세요."),
                 Container(height:30,)
               ],
