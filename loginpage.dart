0e2fe4397ef2b22b7329643986844f04f09103b7
juhojang <juhojang@hotmail.com>
2022-05-07 16:25:41 +0900

    add global key and a little bit icon

--- loginpage.dart
+++ loginpage.dart
@@ -3,33 +3,42 @@ import 'package:flutter/material.dart';
 void main()=>runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
+
+  final GlobalKey<FormState> _formKey=GlobalKey<FormState>();
+  final TextEditingController _emailController=TextEditingController();
+  final TextEditingController _passwordController=TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       home: Scaffold(
         body: Stack(
           alignment: Alignment.center,
           children: [
-            Container(color: Colors.red,),
+            Container(color: Colors.lightBlueAccent,),
             Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Container(width: 200,height: 200,color: Colors.blue,),
                 Stack(children: [
-                  Form(child: Column(
+                  Form(
+                    key: _formKey,
+                      child: Column(
                     children: [
-                      TextFormField(),
-                      TextFormField(),
+                      TextFormField(
+                        controller: _emailController,
+                        decoration: InputDecoration(icon: Icon(Icons.account_circle),labelText: "Email"),
+                      ),
+                      TextFormField(
+                        controller: _passwordController,
+                      ),
                       Container(height: 20,),
                       Text("비밀번호를 잊어버리셨나요?"),
                     ],
                   )),
-                  Container(width: 100,height: 50,color: Colors.black,),
+                  Container(width: 100,height: 50),
                 ],),
                 Container(height:50,),
                 Text("계정이 없으신가요? 하나 만들어보세요."),
