39ab57006e2eb299004ba4da1a2fbe412880c1e6
junhoprog <kimjunho1231@naver.com>
2022-05-11 12:08:15 +0900

    mainpage design

--- lib/main.dart
+++ lib/main.dart
@@ -24,53 +24,70 @@ class Rabbitcalmain extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return Scaffold(
-        appBar:AppBar(
-          title:Text('Rabbitcal',style:TextStyle(
-            fontWeight:FontWeight.bold,
-            fontSize: 40,
-          ),
-          ),
-          centerTitle:true,
+      appBar:AppBar(
+        title:Text('Rabbitcal',style:TextStyle(
+          fontWeight:FontWeight.bold,
+          fontSize: 40,
         ),
-        body: Center(
+        ),
+        centerTitle:true,
+      ),
+      body: Center(
         child: Container(
-        padding: EdgeInsets.all(10),
-    margin:EdgeInsets.all(10),
-    child: Column(
-    children:<Widget>[
-    Container(
-    child: Image.asset('assets/rabbitcal.jpg',width:300,height:300)
-    ),
+          padding: EdgeInsets.all(10),
+          margin:EdgeInsets.all(10),
+          child: Column(
+              children:<Widget>[
+                Container(
+                    child: Image.asset('assets/rabbitcal.jpg',width:300,height:300)
+                ),
 
-    Container(
-    width:250,height:80,
-    padding: EdgeInsets.all(10),
-    margin:EdgeInsets.all(10),
-    child: RaisedButton(
-    child: Text('인바디 등록',style:TextStyle(
-    fontWeight:FontWeight.bold,
-    fontSize: 30,
-    ),),
-    color:Colors.white,
-    ),
-    shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(20),
-    ),)
-
-    Container(
-    width:250,height:80,
-    padding: EdgeInsets.all(10),
-    margin:EdgeInsets.all(10),
-    child: RaisedButton(
-    child: Text('운동 추천',style:TextStyle(
-    fontWeight:FontWeight.bold,
-    fontSize: 30,
-    ),),
-
-    color:Colors.white,
-    ),
-
-    shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(20),
-    ),)
-
+                Container(
+                  width:250,height:80,
+                  padding: EdgeInsets.all(10),
+                  margin:EdgeInsets.all(10),
+                  child: RaisedButton(
+                      child: Text('인바디 등록',style:TextStyle(
+                        fontWeight:FontWeight.bold,
+                        fontSize: 30,
+                      ),),
+                      color:Colors.white,
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(builder:(context)=>Rabbitcal_regitstraion())
+                        );
+                      },
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      )
+                  ),
+                ),
+                Container(
+                  width:250,height:80,
+                  padding: EdgeInsets.all(10),
+                  margin:EdgeInsets.all(10),
+                  child: RaisedButton(
+                      child: Text('운동 추천',style:TextStyle(
+                        fontWeight:FontWeight.bold,
+                        fontSize: 30,
+                      ),),
+                      color:Colors.white,
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(builder:(context)=>Rabbitcal_Recommendation())
+                        );
+                      },
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      )
+                  ),
+                ),
+              ]
+          ),
+        ),
+      ),
+    );
+  }
+}
