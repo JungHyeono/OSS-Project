9d4522020211ea74b550dcb6422c62ab73d001f9
junhoprog <kimjunho1231@naver.com>
2022-05-11 12:02:21 +0900

    create button widget

--- lib/main.dart
+++ lib/main.dart
@@ -34,11 +34,32 @@ class Rabbitcalmain extends StatelessWidget {
         ),
         body: Center(
         child: Container(
+        padding: EdgeInsets.all(10),
+    margin:EdgeInsets.all(10),
     child: Column(
     children:<Widget>[
     Container(
     child: Image.asset('assets/rabbitcal.jpg',width:300,height:300)
     ),
+
+    Container(
+    width:250,height:80,
+    padding: EdgeInsets.all(10),
+    margin:EdgeInsets.all(10),
+    child: RaisedButton(
+    child: Text('인바디 등록',style:TextStyle(
+    fontWeight:FontWeight.bold,
+    fontSize: 30,
+    ),),
     ),
-    ),
-    ),
+
+    Container(
+    width:250,height:80,
+    padding: EdgeInsets.all(10),
+    margin:EdgeInsets.all(10),
+    child: RaisedButton(
+    child: Text('운동 추천',style:TextStyle(
+    fontWeight:FontWeight.bold,
+    fontSize: 30,
+    ),),)
+
